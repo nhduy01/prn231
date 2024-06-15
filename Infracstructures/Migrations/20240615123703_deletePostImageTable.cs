fe305501122eacda521871b9f908b576a334212d
NhaPhan <phanthanhnha06@gmail.com>
2024-06-15 19:39:12 +0700

    Delete Table PostImage

---
+++ Infracstructures/Migrations/20240615123703_deletePostImageTable.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infracstructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletePostImageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostImage");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Image",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PostId",
+                table: "Image",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_PostId",
+                table: "Image",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Image_Post_PostId",
+                table: "Image",
+                column: "PostId",
+                principalTable: "Post",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Image_Post_PostId",
+                table: "Image");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Image_PostId",
+                table: "Image");
+
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "Image");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Image",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "PostImage",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    ImageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PostId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false, defaultValue: "False"),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UpdatedTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostImage_Image_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Image",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_PostImage_Post_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Post",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostImage_ImageId",
+                table: "PostImage",
+                column: "ImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostImage_PostId",
+                table: "PostImage",
+                column: "PostId");
+        }
+    }
+}
