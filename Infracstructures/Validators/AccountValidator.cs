212652f1729fc4748d758d49b72c6b744b0e50d2
nhduy01 <nhduy2001@gmail.com>
2024-07-07 14:13:26 +0700

    fix Create Round

---
+++ Infracstructures/Validators/AccountValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.IValidators;
+using FluentValidation;
+
+namespace Infracstructures.Validators
+{
+    public class AccountValidator : IAccountValidator 
+    {
+        public AccountValidator()
+        {
+            
+        }
+    }
+}
