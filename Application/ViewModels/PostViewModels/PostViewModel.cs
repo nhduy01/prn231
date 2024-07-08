8b721630670613f16110eb5ec36e9f3bdddd0928
nhduy01 <nhduy2001@gmail.com>
2024-07-09 00:10:19 +0700

    Fix CategoryVM + PostVM

--- Application/ViewModels/PostViewModels/PostViewModel.cs
+++ Application/ViewModels/PostViewModels/PostViewModel.cs
@@ -5,6 +5,7 @@ public class PostViewModel
     public Guid Id { get; set; }
     public string Url { get; set; }
     public string Title { get; set; }
+    public string Description { get; set; }
     public Guid? StaffId { get; set; }
     public Guid CategoryId { get; set; }
     public String CategoryName {  get; set; }
