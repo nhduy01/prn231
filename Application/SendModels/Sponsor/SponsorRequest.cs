1831b0a53999a753d586efeab9f0fce3e7524e59
nhduy01 <nhduy2001@gmail.com>
2024-07-15 18:04:48 +0700

    Add logo to sponsor request

--- Application/SendModels/Sponsor/SponsorRequest.cs
+++ Application/SendModels/Sponsor/SponsorRequest.cs
@@ -5,6 +5,7 @@ public class SponsorRequest
     public string Name { get; set; }
     public string Address { get; set; }
     public string Delegate { get; set; }
+    public string Logo { get; set; }
     public string PhoneNumber { get; set; }
     public Guid CurrentUserId { get; set; }
 }
