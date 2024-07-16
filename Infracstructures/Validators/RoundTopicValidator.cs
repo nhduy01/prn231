fc58834c709d9eb17d112e9fe76ee98ba1e42f81
nhduy01 <nhduy2001@gmail.com>
2024-07-16 15:11:54 +0700

    Add RoundTopicValidator

--- Infracstructures/Validators/RoundTopicValidator.cs
+++ Infracstructures/Validators/RoundTopicValidator.cs
@@ -7,12 +7,15 @@ namespace Infracstructures.Validators
     public class RoundTopicValidator : IRoundTopicValidator
     {
         private readonly IValidator<RoundTopicRequest> _roundtopicvalidator;
+        private readonly IValidator<RoundTopicDeleteRequest> _roundtopicdeletevalidator;
 
-        public RoundTopicValidator(IValidator<RoundTopicRequest> roundtopicvalidator)
+        public RoundTopicValidator(IValidator<RoundTopicRequest> roundtopicvalidator, IValidator<RoundTopicDeleteRequest> roundtopicdeletevalidator)
         {
             _roundtopicvalidator = roundtopicvalidator;
+            _roundtopicdeletevalidator = roundtopicdeletevalidator;
         }
 
         public IValidator<RoundTopicRequest> RoundTopicRequestValidator => _roundtopicvalidator;
+        public IValidator<RoundTopicDeleteRequest> RoundTopicDeleteRequestValidator => _roundtopicdeletevalidator;
     }
 }
---
+++ WebAPI/Validation/RoundTopicValidation/RoundTopicDeleteRequestValidator.cs
@@ -0,0 +1,9 @@
+﻿using Application.SendModels.RoundTopic;
+using FluentValidation;
+
+namespace WebAPI.Validation.RoundTopicValidation
+{
+    public class RoundTopicDeleteRequestValidator : AbstractValidator<RoundTopicDeleteRequest>
+    {
+    }
+}
