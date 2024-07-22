9506710fa5161c5f58a5c7d5c5d468cc417f455c
nhduy01 <nhduy2001@gmail.com>
2024-07-22 20:34:29 +0700

    validator

--- WebAPI/Validation/EducationalLevelValidation/EducationalLevelUpdateRequestValidator.cs
+++ WebAPI/Validation/EducationalLevelValidation/EducationalLevelUpdateRequestValidator.cs
@@ -5,4 +5,16 @@ namespace WebAPI.Validation.EducationalLevelValidation;
 
 public class EducationalLevelUpdateRequestValidator : AbstractValidator<EducationalLevelUpdateRequest>
 {
+    public EducationalLevelUpdateRequestValidator()
+    {
+        // Validate ContestId
+        RuleFor(x => x.ContestId)
+            .NotEmpty().WithMessage("ContestId is required.")
+            .NotEqual(Guid.Empty).WithMessage("ContestId must be a valid GUID.");
+
+        // Validate CurrentUserId
+        RuleFor(x => x.CurrentUserId)
+            .NotEmpty().WithMessage("CurrentUserId is required.")
+            .NotEqual(Guid.Empty).WithMessage("CurrentUserId must be a valid GUID.");
+    }
 }
